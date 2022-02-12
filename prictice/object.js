4f8282eb1d9fbaf8a3e82e7cc0dbc48cd6a9f87f
Intizom <asadullohruziev@gmail.com>
2022-02-12 16:26:36 +0500

    Azizbek Khabibullayev lesson 16 is finishid    😎

--- prictice/object.js
+++ prictice/object.js
@@ -1,5 +1,7 @@
 // objects
 // 🇺🇿 1️⃣ --> Basic objects
+// primitive vs referance ning farqlari?
+
 
 const user = {
     key: 'value',
@@ -7,4 +9,97 @@ const user = {
     age:20,
     phone: 996795407
 }
+// const user2 = {
+//     key: 'value',
+//     firstName:'Vali',
+//     age:50,
+//     phone: 996795407
+// }
+
+let user2 = user;
+user2.firstName = 'Matilda'
+
+console.log(user2);
 console.log(user);
+
+
+
+
+
+// 2️⃣    --> Object proporties and methods 
+
+const learner = {
+    city: 'Fargana',
+    country: 'Uzbekistan',
+    create: function(){
+        console.log('Creating some issues');  //proporty ko'rinishidagi method
+    },
+    create2(){
+        console.log('Creating some issues'); // method
+    }
+}
+
+console.log(learner);
+
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+let box = {
+    calledName: 'square',
+    color: 'red',
+    number: 5
+}
+
+// Objectga qo'shish
+box.design = 'modern';
+box.isSale = false
+console.log(box);
+
+// Objectdan o'chhirish (delete)
+delete box.color
+delete box.number
+
+console.log(box);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3️⃣ --> Object ichida bor yoki  yo'qligini tekshirish  
+
+const quti = {
+    raqam: 58,
+    color: 'sariq',
+    egasi: 'Jo\'shqin'
+}
+
+
+console.log("raqam" in quti);
+
+console.log(quti);
+
+
+// Modern ES6 javascript features in object
+
+
+let firstName = 'Usmon';
+
+const person = {
+    firstName,  //firstName: firstName, 
+    age:52
+}
+console.log(person);
+
+
+
+
+// Azizbek Khabibullayev lesson 16 is finishid😎
